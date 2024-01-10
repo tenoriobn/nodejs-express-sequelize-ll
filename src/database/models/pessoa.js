762d36a5a146fe0e30cdc7ab2665189331a76076
Bruno Tenório <tenoriobn@gmail.com>
2024-01-10 16:57:55 -0300

    Validação customizada

--- src/database/models/pessoa.js
+++ src/database/models/pessoa.js
@@ -1,4 +1,7 @@
 'use strict';
+
+const isCpfValido = require('../../ultis/validaCpfHelper.js');
+
 const {
   Model
 } = require('sequelize');
@@ -16,7 +19,15 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Pessoa.init({
-    nome: DataTypes.STRING,
+    nome: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [3, 30],
+          msg: 'o campo nome deve ter no mínimo 3 e máximo 30 caracteres'
+        }
+      }
+    },
     email: {
       type: DataTypes.STRING,
       validate: {
@@ -26,7 +37,14 @@ module.exports = (sequelize, DataTypes) => {
         }
       }
     },
-    cpf: DataTypes.STRING,
+    cpf: {
+      type: DataTypes.STRING,
+      validate: {
+        cpfEhValido: (cpf) => {
+          if (!isCpfValido(cpf)) throw new Error('número de CPF inválido');
+        }
+      }
+    },
     ativo: DataTypes.BOOLEAN,
     role: DataTypes.STRING
   }, {
