b4b6cc1b587e3056b781d5d57fdf01fe74408f64
Bruno Tenório <tenoriobn@gmail.com>
2024-01-11 15:54:57 -0300

    Adicionando escopos de associação

--- src/database/models/pessoa.js
+++ src/database/models/pessoa.js
@@ -16,6 +16,10 @@ module.exports = (sequelize, DataTypes) => {
         scope: { status: 'matriculado' },
         as: 'aulasMatriculadas'
       });
+      Pessoa.hasMany(models.Matricula, {
+        foreignKey: 'estudante_id',
+        as: 'todasAsMatriculadas'
+      });
     }
   }
   Pessoa.init({
